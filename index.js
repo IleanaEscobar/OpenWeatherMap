a9a9fb7769d555c159eeb44a2f280071ce810d7f
IleanaEscobar <71234828+IleanaEscobar@users.noreply.github.com>
2020-10-24 16:11:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -68,19 +68,3 @@ class OpenWeather {
 }
 
 module.exports = OpenWeather
-
-const forecast = new OpenWeather('21b4c98c81ab5911942bb0d6fd5ee4c7') //Unique client code used for identification and authorization purposes. Contact AccuWeather to receive an API key.
-  
-forecast
-				.location('London')				// http://apidev.accuweather.com/developers/locationsAPIguide
-			//	.time('hourly/1hour')				// http://apidev.accuweather.com/developers/forecastsAPIguide
-				.language("ru")					// http://apidev.accuweather.com/developers/languages
-				.metric(true)					// Boolean value (true or false) that specifies to return the data in either metric (=true) or imperial units 
-				.details(true)					// Boolean value (true or false) that specifies whether or not to include a truncated version of the forecasts object or the full object (details = true)
-				.get()
-				.then(res => {
-					console.log(res)
-				})
-				.catch(err => {
-					console.log(err)
-				})
