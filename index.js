c89e6c40d48299eeca771bcb1c09680ab4329250
IleanaEscobar <71234828+IleanaEscobar@users.noreply.github.com>
2020-11-09 05:24:43 -0500

    Delete unnecessary code

--- index.js
+++ index.js
@@ -88,21 +88,3 @@ class OpenWeather {
     }
 }
 module.exports = OpenWeather
-
-const forecast = new OpenWeather('21b4c98c81ab5911942bb0d6fd5ee4c7') //Unique client code used for identification and authorization purposes. Contact OpenWeather to receive an API key.
-  
-forecast
-				.location('London')						
-                .details(true)	
-                .excludeDay(true)
-                .imperial(true)
-                .details(true)
-                .language("ru")
-				// Boolean value (true or false) that specifies whether or not to include a truncated version of the forecasts object or the full object (details = true)
-				.get()
-				.then(res => {
-					console.log(res)
-				})
-				.catch(err => {
-					console.log(err)
-				})
